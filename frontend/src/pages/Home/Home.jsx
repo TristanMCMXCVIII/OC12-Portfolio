208eff1d7ae680d5d64507078b3cac03c7d0b3df
Tristan <nocent.tristan@gmail.com>
2024-04-14 18:28:39 +0200

    patch 2

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -26,7 +26,7 @@ const Home = ({projects, feedbacks}) => {
             </section>
             <section className='home__container--column'>
                 <h2>QUI SUIS-JE</h2>
-                <p className='home__speech'>Diplomé d'une école d'ingénieur en 2021, INSA Lyon, je suis passionné par les nouvelles technologies, et en particulier l'informatique et ses sous domaines : site web, jeux, télécommunication, IA... Je suis aussi un ancien élève de l'ESSEC, école de commerce que j'ai intégré à travers le programme Grande Ecole. Mais officiellement, je n'y ai étudié, à ce jours, que 10j car j'ai rejoins un ami d'enfance, entrepreneur, avec lequel nous avons aujourd'hui une agence de développement et un incubateur pour projets informatique, entre autres !</p>
+                <p className='home__speech'>Diplomé d'une école d'ingénieur en 2021, INSA Lyon, je suis passionné par les nouvelles technologies, et en particulier l'informatique et ses sous domaines : site web, jeux, télécommunication, IA... Je suis aussi un ancien élève de l'ESSEC, école de commerce que j'ai intégré à travers le programme Grande Ecole. Mais officiellement, je n'y ai étudié, à ce jours, que 10j car j'ai rejoins un ami d'enfance, entrepreneur, avec lequel nous avons aujourd'hui une agence de développement et un incubateur pour projets informatiques, entre autres !</p>
             </section>
             <section className='home__container--column'>
                 <h2>COMPETENCES</h2>
